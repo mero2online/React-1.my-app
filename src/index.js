2e70c22fb91e3d64f5ae1d4d55157b13848965d7
mero2online <mero2online@gmail.com>
2021-10-17 14:05:14 +0200

    Final files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function getButtonText() {
+//   return 'Click on me!';
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Create a react component
+const App = () => {
+  //   const buttonText = 'Click Me!';
+  //   const buttonText = 123456;
+  //   const buttonText = ['Hi','There'];
+  //   const buttonText = [10, 20];
+  const buttonText = { text: 'Click me' };
+  const labelText = 'Enter Name:';
+  return (
+    <div>
+      <label className='label' htmlFor='name'>
+        {labelText}
+      </label>
+      <input id='name' type='text' />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+if (module.hot) {
+  module.hot.accept();
+}
